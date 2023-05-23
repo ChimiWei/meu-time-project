e8f8f543ae3c0c7becc94d343d175c2e41ca0bd8
Wei Oliveira <87078907+ChimiWei@users.noreply.github.com>
2023-05-23 00:53:50 -0300

    API key setup and hook for fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+import { useFetch } from './hooks/useFetch'
 
 function App() {
-  
-
+  const { data } = useFetch<string>('https://api-football-v1.p.rapidapi.com/v3/teams?id=33')
+  console.log(data)
   return (
     <>
       <div>
         <p> Oi </p>
+        <p>{ data }</p>
       </div>
     </>
   )
---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+export function useFetch<T = unknown>(url: string) {
+const [data, setData] = useState<T | null>(null)
+
+var myHeaders = new Headers();
+myHeaders.append("x-rapidapi-key", import.meta.env.VITE_MY_KEY);
+myHeaders.append("x-rapidapi-host", "api-football-v1.p.rapidapi.com");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders
+};
+
+useEffect(() => {
+    fetch(url, requestOptions)
+    .then(response => response.json())
+    .then(response => {
+        setData(response.response[0].team.name);
+    })
+    .catch(error => console.log('error', error));
+
+}, [])
+return {data}
+}
