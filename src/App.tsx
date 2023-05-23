1c56b4c38f6c39022ad6e3973e366f87988550d3
Wei Oliveira <87078907+ChimiWei@users.noreply.github.com>
2023-05-23 20:06:22 -0300

    styling adjust

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import { useKey } from "./hooks/useKey"
 import { useState } from "react"
 import { CustomInput } from "./components/CustomInput/CustomInput"
+import { Section } from "./layouts/section/Section"
+import { Title } from "./components/Title/Title"
+
 
 function App() {
  const { status, isValid, setIsValid, key, handleSetKey } = useKey()
  console.log(key)
   return (
     <>
+      <Section>
+      <Title>
+        Meu Time
+      </Title>
       <div>
         <CustomInput value={key} setValue={handleSetKey}/> 
       </div>
-      <div>
-        <p> Oi </p>
-      </div>
+      </Section>
     </>
   )
 }
