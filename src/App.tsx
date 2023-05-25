d1ba8328e759df650c8f38c59ac0817a0fdf0195
Wei Oliveira <87078907+ChimiWei@users.noreply.github.com>
2023-05-24 21:06:16 -0300

    keyContext for valid keys

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import { useKey } from "./hooks/useKey"
 import { CustomInput } from "./components/CustomInput/CustomInput"
 import { Section } from "./layouts/section/Section"
 import { Title } from "./components/Title/Title"
+import { useEffect, createContext, useState } from "react"
 
 
+export const KeyContext = createContext<string | null>(null);
 
 function App() {
  const { error, isValid, key, handleSetKey } = useKey()
+ const [validKey, setValidKey] = useState<string>('')
+ 
+ useEffect(() => {
+  setValidKey(key)
+ }, [isValid])
  
   return (
-    <>
+    <KeyContext.Provider value={validKey }>
       <Section>
       <Title>
         Meu Time
@@ -19,10 +26,10 @@ function App() {
         <small>{error}</small>
       </>}
       {isValid == true && <>
-        <p> Login Realizado! </p>
+        <p> Login Realizado com a chave {validKey}! </p>
       </>}
       </Section>
-    </>
+    </ KeyContext.Provider>
   )
 }
 
