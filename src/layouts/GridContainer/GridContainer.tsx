ff11304fc4d264e91b9e816041c9ee0219b910af
Wei Oliveira <87078907+ChimiWei@users.noreply.github.com>
2023-06-29 10:26:23 -0300

    centering adjusts

--- src/layouts/GridContainer/GridContainer.tsx
+++ src/layouts/GridContainer/GridContainer.tsx
@@ -4,15 +4,15 @@ import { styled } from "styled-components";
 
 export const GridContainer = styled.div`
     display: grid;
-    grid-template-columns: 100% 100%;
-    width: 500px;
+    grid-template-columns: 300px 300px;
+    width: 100px;
     padding-top: 50px;
     gap: 10em;
     justify-content: center;
     align-items: center;
     text-align: center;
 
-    @media (max-width:1160px) {
+    @media (max-width:1000px) {
         display: flex;
         flex-direction: column;
         width: 100%;
